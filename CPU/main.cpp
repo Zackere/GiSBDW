32900252f8106501d175a873615f0daf6a1a47ac
replinw <wo1001@wp.pl>
2020-04-21 01:07:14 +0200

    fixup! Add boost graph example

--- CPU/main.cpp
+++ CPU/main.cpp
@@ -1,13 +1,13 @@
 // Copyright 2020 GISBDW. All rights reserved.
 
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/depth_first_search.hpp>
-#include <boost/graph/erdos_renyi_generator.hpp>
-#include <boost/graph/graphviz.hpp>
-#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <random>
+
+#include "boost/graph/adjacency_list.hpp"
+#include "boost/graph/depth_first_search.hpp"
+#include "boost/graph/erdos_renyi_generator.hpp"
+#include "boost/graph/graphviz.hpp"
 
 class VertexVisitor : public boost::default_dfs_visitor {
  public:
@@ -21,9 +21,7 @@ int main() {
   using Graph = boost::adjacency_list<>;
   using ERGen = boost::erdos_renyi_iterator<std::minstd_rand, Graph>;
   int n = 25;
-  std::minstd_rand rng(std::chrono::duration_cast<std::chrono::seconds>(
-                           std::chrono::system_clock::now().time_since_epoch())
-                           .count());
+  std::minstd_rand rng;
   Graph g(ERGen(rng, n, 0.05), ERGen(), n);
   boost::depth_first_search(g, boost::visitor(VertexVisitor()));
   std::ofstream file("graph.gviz", std::ios_base::trunc);
