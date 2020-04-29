f3d825644850eb0f954bef34c692160bcf1e1c97
replinw <wo1001@wp.pl>
2020-04-29 18:51:58 +0200

    Implement most of EliminationTree interface

--- CPU/src/elimination_tree/elimination_tree.cpp
+++ CPU/src/elimination_tree/elimination_tree.cpp
@@ -1,17 +1,59 @@
 #include "elimination_tree.hpp"
 
 namespace td {
-std::set<EliminationTree::VertexType> const&
+EliminationTree::Component::AdjacencyList::mapped_type const&
 EliminationTree::Component::Neighbours(VertexType v) const {
   return neighbours_.find(v)->second;
+}
+
+unsigned EliminationTree::Component::GetDepth() const {
+  return depth_;
 }
 
 void EliminationTree::Eliminate(VertexType v) {}
 
 void EliminationTree::Merge(VertexType v) {}
 
-EliminationTree::Component const& EliminationTree::GetComponent(
-    VertexType v) const {
-  return components_.find(std::get<ComponentIndex>(nodes_[v].children))->second;
+EliminationTree::ComponentIterator EliminationTree::ComponentsBegin() const {
+  return ComponentIterator{std::cbegin(components_)};
 }
+
+EliminationTree::ComponentIterator EliminationTree::ComponentsEnd() const {
+  return ComponentIterator{std::cend(components_)};
+}
+
+EliminationTree::Component const&
+EliminationTree::ComponentIterator::operator*() const {
+  return cur_->second;
+}
+
+EliminationTree::Component const*
+EliminationTree::ComponentIterator::operator->() const {
+  return &cur_->second;
+}
+
+EliminationTree::ComponentIterator&
+EliminationTree::ComponentIterator::operator++() {
+  ++cur_;
+  return *this;
+}
+
+EliminationTree::ComponentIterator&
+EliminationTree::ComponentIterator::operator--() {
+  --cur_;
+  return *this;
+}
+
+bool EliminationTree::ComponentIterator::operator==(
+    ComponentIterator const& other) {
+  return cur_ == other.cur_;
+}
+
+bool EliminationTree::ComponentIterator::operator!=(
+    ComponentIterator const& other) {
+  return cur_ != other.cur_;
+}
+
+EliminationTree::ComponentIterator::ComponentIterator(Iterator init)
+    : cur_{init} {}
 }  // namespace td
--- CPU/src/elimination_tree/elimination_tree.hpp
+++ CPU/src/elimination_tree/elimination_tree.hpp
@@ -3,44 +3,87 @@
 
 #include <map>
 #include <set>
+#ifdef TD_CHECK_ARGS
+#include <stdexcept>
+#endif
 #include <variant>
 #include <vector>
 
 #include "boost/graph/adjacency_list.hpp"
+#include "boost/graph/connected_components.hpp"
 
 namespace td {
 class EliminationTree {
  public:
-  using Graph =
-      boost::adjacency_list<boost::mapS, boost::vecS, boost::undirectedS>;
-  using VertexType = int;
-  using ComponentIndex = int;
+  using VertexType = std::size_t;
+  using ComponentIndex = std::size_t;
   class Component {
-    std::set<VertexType> const& Neighbours(VertexType v) const;
+   public:
+    using AdjacencyList = std::map<VertexType, std::set<VertexType>>;
+    AdjacencyList::mapped_type const& Neighbours(VertexType v) const;
+    unsigned GetDepth() const;
 
    private:
-    std::map<VertexType, std::set<VertexType>> neighbours_;
+    AdjacencyList neighbours_;
+    unsigned depth_;
+  };
+  class ComponentIterator {
+   public:
+    ComponentIterator(ComponentIterator const&) = default;
+    ComponentIterator(ComponentIterator&&) = default;
+    ComponentIterator& operator=(ComponentIterator const&) = default;
+    ComponentIterator& operator=(ComponentIterator&&) = default;
+    ~ComponentIterator() = default;
+
+    Component const& operator*() const;
+    Component const* operator->() const;
+
+    ComponentIterator& operator++();
+    ComponentIterator& operator--();
+
+    bool operator==(ComponentIterator const& other);
+    bool operator!=(ComponentIterator const& other);
+
+   private:
+    friend class EliminationTree;
+    using Iterator = std::map<ComponentIndex, Component>::const_iterator;
+    ComponentIterator(Iterator init);
+    Iterator cur_;
   };
 
-  template <typename G>
-  EliminationTree(G const& g);
-
+  template <typename OutEdgeList, typename VertexList, typename... Args>
+  EliminationTree(boost::adjacency_list<OutEdgeList,
+                                        VertexList,
+                                        boost::undirectedS,
+                                        Args...> const& g);
   void Eliminate(VertexType v);
   void Merge(VertexType v);
-  Component const& GetComponent(VertexType v) const;
-  // TODO(replinw): implement possibility of iterating over component
+
+  ComponentIterator ComponentsBegin() const;
+  ComponentIterator ComponentsEnd() const;
 
  private:
   struct Node {
-    std::variant<std::vector<Node>, ComponentIndex> children;
+    std::variant<std::pair<std::vector<Node>, unsigned>, ComponentIndex>
+        children;
   };
-  VertexType root_;
   std::vector<Node> nodes_;
   std::map<ComponentIndex, Component> components_;
-  Graph original_graph_;
-  std::vector<VertexType> eliminated_;
+  std::vector<Component::AdjacencyList::node_type> eliminated_;
 };
 
-template <typename G>
-EliminationTree::EliminationTree(G const& g) {}
+template <typename OutEdgeList, typename VertexList, typename... Args>
+EliminationTree::EliminationTree(boost::adjacency_list<OutEdgeList,
+                                                       VertexList,
+                                                       boost::undirectedS,
+                                                       Args...> const& g)
+    : nodes_(boost::num_vertices(g)) {
+#ifdef TD_CHECK_ARGS
+  if (boost::connected_components(
+          g, std::vector<int>(boost::num_vertices(g)).data()) != 1)
+    throw std::invalid_argument(
+        "EliminationTree works only on connected graphs");
+#endif
+  eliminated_.reserve(boost::num_vertices(g));
+}
 }  // namespace td
