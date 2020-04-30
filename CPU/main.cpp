6eb43ba710fa4ca99810961eb7fd51512c4ca701
replinw <wo1001@wp.pl>
2020-04-30 15:12:05 +0200

    Shut cpplint up

--- CPU/main.cpp
+++ CPU/main.cpp
@@ -10,7 +10,6 @@
 #include "boost/graph/depth_first_search.hpp"
 #include "boost/graph/erdos_renyi_generator.hpp"
 #include "boost/graph/graphviz.hpp"
-#include "src/branch_and_bound/branch_and_bound.hpp"
 
 struct VertexVisitor : public boost::default_dfs_visitor {
   template <typename Vertex, typename Graph>
@@ -24,11 +23,10 @@ int main() {
       boost::adjacency_list<boost::mapS, boost::vecS, boost::undirectedS>;
   using ERGen = boost::sorted_erdos_renyi_iterator<std::minstd_rand, Graph>;
   constexpr int n = 15;
-  using namespace std::chrono;
-  std::minstd_rand rng(
-      duration_cast<seconds>(system_clock::now().time_since_epoch()).count());
+  std::minstd_rand rng;
   Graph g(ERGen(rng, n, 0.2), ERGen(), n);
   boost::depth_first_search(g, boost::visitor(VertexVisitor()));
+  std::cout << std::endl;
   std::ofstream file("graph.gviz", std::ios_base::trunc);
   boost::write_graphviz(file, g);
   file.close();
