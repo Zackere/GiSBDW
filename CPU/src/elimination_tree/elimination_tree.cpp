7042a67dd13907942cf92158e8bd6a1b773dfe73
replinw <wo1001@wp.pl>
2020-04-30 03:46:07 +0200

    Implement EliminationTree::Eliminate

--- CPU/src/elimination_tree/elimination_tree.cpp
+++ CPU/src/elimination_tree/elimination_tree.cpp
@@ -7,11 +7,46 @@ EliminationTree::Component::Neighbours(VertexType v) const {
   return neighbours_.find(v)->second;
 }
 
-unsigned EliminationTree::Component::GetDepth() const {
+unsigned EliminationTree::Component::Depth() const {
   return depth_;
 }
 
-void EliminationTree::Eliminate(VertexType v) {}
+void EliminationTree::Eliminate(VertexType v) {
+  auto& v_node = nodes_[v]->v;
+  auto& v_component = std::get<Component>(v_node);
+  eliminated_.emplace_back(v_component.neighbours_.extract(v));
+  for (auto& p : v_component.neighbours_)
+    p.second.erase(v);
+  auto new_v_node = std::make_pair(std::list<Node>{}, v_component.Depth());
+  while (!v_component.neighbours_.empty()) {
+    Node new_node;
+    new_node.v = Component();
+    auto& new_component = std::get<Component>(new_node.v);
+    new_component.depth_ = v_component.Depth() + 1;
+    auto it = new_component.neighbours_
+                  .insert(v_component.neighbours_.extract(
+                      std::begin(v_component.neighbours_)))
+                  .position;
+    while (it != std::end(new_component.neighbours_)) {
+      for (auto neigh = std::begin(v_component.neighbours_);
+           neigh != std::end(v_component.neighbours_);)
+        if (neigh->second.find(it->first) != std::end(neigh->second)) {
+          new_component.neighbours_[neigh->first] = std::move(neigh->second);
+          neigh = v_component.neighbours_.erase(neigh);
+        } else {
+          ++neigh;
+        }
+      ++it;
+    }
+    new_v_node.first.push_back(std::move(new_node));
+    auto& back_ref = std::get<Component>(new_v_node.first.back().v);
+    for (auto& p : back_ref.neighbours_)
+      nodes_[p.first] = &new_v_node.first.back();
+    components_.insert(&back_ref);
+  }
+  components_.erase(&v_component);
+  v_node = std::move(new_v_node);
+}
 
 void EliminationTree::Merge(VertexType v) {}
 
@@ -25,36 +60,36 @@ EliminationTree::ComponentIterator EliminationTree::ComponentsEnd() const {
 
 EliminationTree::Component const&
 EliminationTree::ComponentIterator::operator*() const {
-  return cur_->second;
+  return **current_;
 }
 
 EliminationTree::Component const*
 EliminationTree::ComponentIterator::operator->() const {
-  return &cur_->second;
+  return *current_;
 }
 
 EliminationTree::ComponentIterator&
 EliminationTree::ComponentIterator::operator++() {
-  ++cur_;
+  ++current_;
   return *this;
 }
 
 EliminationTree::ComponentIterator&
 EliminationTree::ComponentIterator::operator--() {
-  --cur_;
+  --current_;
   return *this;
 }
 
 bool EliminationTree::ComponentIterator::operator==(
     ComponentIterator const& other) {
-  return cur_ == other.cur_;
+  return current_ == other.current_;
 }
 
 bool EliminationTree::ComponentIterator::operator!=(
     ComponentIterator const& other) {
-  return cur_ != other.cur_;
+  return current_ != other.current_;
 }
 
 EliminationTree::ComponentIterator::ComponentIterator(Iterator init)
-    : cur_{init} {}
+    : current_{init} {}
 }  // namespace td
--- CPU/src/elimination_tree/elimination_tree.hpp
+++ CPU/src/elimination_tree/elimination_tree.hpp
@@ -1,14 +1,16 @@
 // Copyright 2020 GISBDW. All rights reserved.
 #pragma once
 
+#include <list>
 #include <map>
 #include <set>
-#ifdef TD_CHECK_ARGS
-#include <stdexcept>
-#endif
+#include <type_traits>
 #include <utility>
 #include <variant>
 #include <vector>
+#ifdef TD_CHECK_ARGS
+#include <stdexcept>
+#endif
 
 #include "boost/graph/adjacency_list.hpp"
 #include "boost/graph/connected_components.hpp"
@@ -22,20 +24,15 @@ class EliminationTree {
    public:
     using AdjacencyList = std::map<VertexType, std::set<VertexType>>;
     AdjacencyList::mapped_type const& Neighbours(VertexType v) const;
-    unsigned GetDepth() const;
+    unsigned Depth() const;
 
    private:
+    friend class EliminationTree;
     AdjacencyList neighbours_;
     unsigned depth_;
   };
   class ComponentIterator {
    public:
-    ComponentIterator(ComponentIterator const&) = default;
-    ComponentIterator(ComponentIterator&&) = default;
-    ComponentIterator& operator=(ComponentIterator const&) = default;
-    ComponentIterator& operator=(ComponentIterator&&) = default;
-    ~ComponentIterator() = default;
-
     Component const& operator*() const;
     Component const* operator->() const;
 
@@ -47,9 +44,9 @@ class EliminationTree {
 
    private:
     friend class EliminationTree;
-    using Iterator = std::map<ComponentIndex, Component>::const_iterator;
+    using Iterator = std::set<Component*>::const_iterator;
     explicit ComponentIterator(Iterator init);
-    Iterator cur_;
+    Iterator current_;
   };
 
   template <typename OutEdgeList, typename VertexList, typename... Args>
@@ -65,12 +62,14 @@ class EliminationTree {
 
  private:
   struct Node {
-    std::variant<std::pair<std::vector<Node>, unsigned>, ComponentIndex>
-        children;
-  };
-  std::vector<Node> nodes_;
-  std::map<ComponentIndex, Component> components_;
+    std::variant<std::pair<std::list<Node>, unsigned>, Component> v;
+  } root_;
+  std::vector<Node*> nodes_;
+  std::set<Component*> components_;
   std::vector<Component::AdjacencyList::node_type> eliminated_;
+
+  EliminationTree(EliminationTree const&) = delete;
+  EliminationTree& operator=(EliminationTree const&) = delete;
 };
 
 template <typename OutEdgeList, typename VertexList, typename... Args>
@@ -78,13 +77,26 @@ EliminationTree::EliminationTree(boost::adjacency_list<OutEdgeList,
                                                        VertexList,
                                                        boost::undirectedS,
                                                        Args...> const& g)
-    : nodes_(boost::num_vertices(g)) {
+    : nodes_(boost::num_vertices(g), &root_) {
 #ifdef TD_CHECK_ARGS
   if (boost::connected_components(
           g, std::vector<int>(boost::num_vertices(g)).data()) != 1)
     throw std::invalid_argument(
         "EliminationTree works only on connected graphs");
+  for (int i = 0; i < boost::num_vertices(g); ++i)
+    if (boost::edge(i, i, g).second)
+      throw std::invalid_argument("Self loops are not allowed");
 #endif
   eliminated_.reserve(boost::num_vertices(g));
+  Component root;
+  root.depth_ = 0;
+  typename boost::graph_traits<
+      std::remove_reference_t<decltype(g)>>::out_edge_iterator ei,
+      ei_end;
+  for (int i = 0; i < boost::num_vertices(g); ++i)
+    for (boost::tie(ei, ei_end) = out_edges(i, g); ei != ei_end; ++ei)
+      root.neighbours_[i].insert(target(*ei, g));
+  root_.v = std::move(root);
+  components_.insert(&std::get<Component>(root_.v));
 }
 }  // namespace td
