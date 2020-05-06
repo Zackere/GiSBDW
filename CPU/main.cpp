bb63c7206534656db4d5a3ac4cdcdd9000310334
Bartosz Rozanski <bartosz.rozanski11@gmail.com>
2020-05-06 18:54:19 +0200

    Test commit

--- CPU/main.cpp
+++ CPU/main.cpp
@@ -1,13 +1,18 @@
 // Copyright 2020 GISBDW. All rights reserved.
 
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <random>
+#include <string>
 
 #include "boost/graph/adjacency_list.hpp"
 #include "boost/graph/depth_first_search.hpp"
 #include "boost/graph/erdos_renyi_generator.hpp"
 #include "boost/graph/graphviz.hpp"
+#include "boost/program_options.hpp"
+
+namespace po = boost::program_options;
 
 class VertexVisitor : public boost::default_dfs_visitor {
  public:
@@ -17,7 +22,26 @@ class VertexVisitor : public boost::default_dfs_visitor {
   }
 };
 
-int main() {
+void usage(po::options_description const& description) {
+  std::cout << description;
+  std::exit(1);
+}
+
+int main(int argc, char** argv) {
+  po::options_description description("Allowed options");
+  description.add_options()("help", "produce help message")(
+      "algorithm,a", po::value<std::string>(), "set compression level")(
+      "input,i", po::value<std::string>(), "path to input graph")(
+      "output,o", po::value<std::string>(), "path to output dir");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, description), vm);
+  po::notify(vm);
+
+  if (!vm.count("help")) {
+    usage(description);
+  }
+
   using Graph = boost::adjacency_list<>;
   using ERGen = boost::erdos_renyi_iterator<std::minstd_rand, Graph>;
   int n = 25;
