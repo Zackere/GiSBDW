5245c4b878884802da0a89a14e98de1c2f57fc90
replinw <wo1001@wp.pl>
2020-05-11 20:31:15 +0200

    Implement map encoding

--- Treedepth/src/set_encoder/set_encoder.hpp
+++ Treedepth/src/set_encoder/set_encoder.hpp
@@ -6,6 +6,7 @@
 #define HD __host__ __device__
 #endif
 
+#include <map>
 #include <set>
 
 namespace td {
@@ -13,6 +14,15 @@ namespace set_encoder {
 HD std::size_t NChooseK(int n, int k);
 
 std::size_t Encode(std::set<int8_t> const& s);
+
+template <typename... Args>
+std::size_t Encode(std::map<Args...> const& map) {
+  std::size_t ret = 0;
+  int i = 0;
+  for (auto v : map)
+    ret += NChooseK(v.first, ++i);
+  return ret;
+}
 
 HD std::size_t Encode(int8_t* sorted_set, int k);
 
